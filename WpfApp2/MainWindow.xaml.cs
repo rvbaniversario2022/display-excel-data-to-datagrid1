0ae1f91315f39b4b612baa15c29744f434c67676
rvbaniversario2022 <rvbaniversario.itd.pps@gmail.com>
2022-10-31 16:19:34 +0800

    first commit

---
+++ WpfApp2/MainWindow.xaml.cs
@@ -0,0 +1,141 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnOpen_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openfile = new OpenFileDialog();
+            openfile.DefaultExt = ".xlsx";
+            openfile.Filter = "(.xlsx)|*.xlsx";
+            //openfile.ShowDialog();
+
+            var browsefile = openfile.ShowDialog();
+
+            if (browsefile == true)
+            {
+                TextFilePath.Text = openfile.FileName;
+
+                Microsoft.Office.Interop.Excel.Application excelApp = new Microsoft.Office.Interop.Excel.Application();
+                //Static File From Base Path...........
+                //Microsoft.Office.Interop.Excel.Workbook excelBook = excelApp.Workbooks.Open(AppDomain.CurrentDomain.BaseDirectory + "TestExcel.xlsx", 0, true, 5, "", "", true, Microsoft.Office.Interop.Excel.XlPlatform.xlWindows, "\t", false, false, 0, true, 1, 0);
+                //Dynamic File Using Uploader...........
+                Microsoft.Office.Interop.Excel.Workbook excelBook = excelApp.Workbooks.Open(TextFilePath.Text.ToString(), 0, true, 5, "", "", true, Microsoft.Office.Interop.Excel.XlPlatform.xlWindows, "\t", false, false, 0, true, 1, 0);
+                Microsoft.Office.Interop.Excel.Worksheet excelSheet = (Microsoft.Office.Interop.Excel.Worksheet)excelBook.Worksheets.get_Item(1); ;
+                Microsoft.Office.Interop.Excel.Range excelRange = excelSheet.UsedRange;
+
+                string strCellData = "";
+                double douCellData;
+                int rowCnt = 0;
+                int colCnt = 0;
+
+                DataTable dt = new DataTable();
+                for (colCnt = 1; colCnt <= excelRange.Columns.Count; colCnt++)
+                {
+                    string strColumn = "";
+                    strColumn = (string)(excelRange.Cells[1, colCnt] as Microsoft.Office.Interop.Excel.Range).Value2;
+                    dt.Columns.Add(strColumn, typeof(string));
+                }
+
+
+                for (rowCnt = 2; rowCnt <= excelRange.Rows.Count; rowCnt++)
+                {
+                    string strData = "";
+                    for (colCnt = 1; colCnt <= excelRange.Columns.Count; colCnt++)
+                    {
+                        try
+                        {
+                            strCellData = (string)(excelRange.Cells[rowCnt, colCnt] as Microsoft.Office.Interop.Excel.Range).Value2;
+                            strData += strCellData + "|";
+                        }
+                        catch (Exception)
+                        {
+                            douCellData = (excelRange.Cells[rowCnt, colCnt] as Microsoft.Office.Interop.Excel.Range).Value2;
+                            strData += douCellData.ToString() + "|";
+                        }
+                    }
+                    strData = strData.Remove(strData.Length - 1, 1);
+                    dt.Rows.Add(strData.Split('|'));
+                }
+
+                DataGrid.ItemsSource = dt.DefaultView;
+
+                excelBook.Close(true, null, null);
+                excelApp.Quit();
+
+            }
+        }
+
+        private void BtnSearch_Click(object sender, EventArgs e)
+        {
+            //try
+            //{
+            //    DataView dv = DataGrid.ItemsSource as DataView;
+            //    if (dv != null)
+            //        dv.RowFilter = TextSearch.Text;
+            //}
+            //catch (Exception ex)
+            //{
+            //    MessageBox.Show(ex.Message, "Message", MessageBoxButton.OK, MessageBoxImage.Error);
+            //}
+        }
+
+        private void TextSearch_KeyDown(object sender, KeyEventArgs e)
+        {
+        }
+    }
+}
+
+//private void TextSearch_KeyDown(object sender, KeyEventArgs e)
+//{
+//    Key k = e.Key;
+
+//    // to see the key enums displayed, use a textbox or label
+//    // someTextBox.Text = k.ToString();
+
+//    // filter out control keys, not all are used, add more as needed
+//    bool controlKeyIsDown = Keyboard.IsKeyDown(Key.LeftShift);
+
+//    if (!controlKeyIsDown &&
+//        Key.D0 <= k && k <= Key.D9 ||
+//        Key.NumPad0 <= k && k <= Key.NumPad9 ||
+//        k == Key.OemMinus || k == Key.Subtract ||
+//        k == Key.Decimal || k == Key.OemPeriod)   // or OemComma for european decimal point
+//    {
+//        BtnSearch.RaiseEvent(new RoutedEventArgs(Button.ClickEvent, BtnSearch));
+//    }
+
+//    else
+//    {
+//        e.Handled = true;
+
+//        // just a little sound effect for wrong key pressed
+//        System.Media.SystemSound ss = System.Media.SystemSounds.Beep;
+//        ss.Play();
+
+//    }
+//}
